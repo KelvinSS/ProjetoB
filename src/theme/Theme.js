68b816b316389fcf18b97c2f11f32edc2ef6f6d3
KelvinSS <kelvimcyndder@gmail.com>
2024-09-14 22:38:53 -0300

    nova padrao de cores

---
+++ src/theme/Theme.js
@@ -0,0 +1,16 @@
+
+export const COLOR = {
+    White: '#fff',
+    Black: '#333',
+
+    Grey: '#ccc',
+
+    Red: '#fa1505',
+
+    Gold1: '#ffd700',
+    Gold2: '#daa520',
+
+    Jade: '#00A86B',
+
+    Background: '#f7f7f7'
+};
