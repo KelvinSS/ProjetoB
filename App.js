845a410d67bacce652402da22b1e6a9ec783ee38
KelvinSS <kelvimcyndder@gmail.com>
2024-09-14 22:37:46 -0300

    novas paginas e novas cores

--- App.js
+++ App.js
@@ -11,16 +11,26 @@ import ConfigScreen from './src/screens/ConfigScreen';
 import CreditExpensesScreen from './src/screens/CreditExpensesScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import PlanningScreen from './src/screens/PlanningScreen';
+import AddRecurringExpenseScreen from './src/screens/AddRecurringExpenseScreen';
+import EditPlanningScreen from './src/screens/EditPlanningScreen';
+
 import { AuthProvider, AuthContext } from './src/context/authContext';
+import { PlanningProvider } from './src/context/PlanningContext';
 import { ExpenseProvider } from './src/context/ExpenseContext';
+import { COLOR } from './src/theme/Theme';
 
 const Stack = createStackNavigator();
 
 function AppNavigator() {
-  const { isLoggedIn } = useContext(AuthContext); // Acessa o estado de login do AuthContext
+  const { isLoggedIn } = useContext(AuthContext);
 
   return (
-    <Stack.Navigator initialRouteName={isLoggedIn ? 'Home' : 'Login'} screenOptions={{ headerStyle: { backgroundColor: '#ccc' } }}>
+    <Stack.Navigator
+      initialRouteName={isLoggedIn ? 'Home' : 'Login'}
+      screenOptions={{
+        headerStyle: { backgroundColor: COLOR.Jade },
+        headerTintColor: COLOR.White,
+      }}>
       <Stack.Screen name="Home" component={HomeScreen} options={{ header: () => null }} />
       <Stack.Screen name="AddExpense" component={AddExpenseScreen} options={{ title: 'Despesa' }} />
       <Stack.Screen name="EditExpense" component={EditExpenseScreen} options={{ title: 'Editar despesa' }} />
@@ -28,6 +38,8 @@ function AppNavigator() {
       <Stack.Screen name="CreditExpenses" component={CreditExpensesScreen} options={{ title: 'Fatura do cartão' }} />
       <Stack.Screen name="PlanningScreen" component={PlanningScreen} options={{ title: 'Planejamento' }} />
       <Stack.Screen name="Login" component={LoginScreen} options={{ header: () => null }} />
+      <Stack.Screen name="AddRecurringExpense" component={AddRecurringExpenseScreen} options={{ title: 'Gasto recorrente' }} />
+      <Stack.Screen name="EditPlanningScreen" component={EditPlanningScreen} options={{ title: 'Editar Gasto recorrente' }} />
     </Stack.Navigator>
   );
 }
@@ -57,10 +69,12 @@ export default function App() {
   return (
     <AuthProvider>
       <ExpenseProvider>
-        <NavigationContainer>
-          <StatusBar barStyle={'dark-content'} backgroundColor={'#ccc'} />
-          <AppNavigator />
-        </NavigationContainer>
+        <PlanningProvider>
+          <NavigationContainer>
+            <StatusBar barStyle={'light-content'} backgroundColor={COLOR.Jade} />
+            <AppNavigator />
+          </NavigationContainer>
+        </PlanningProvider>
       </ExpenseProvider>
     </AuthProvider>
   );
