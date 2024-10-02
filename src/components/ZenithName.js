c9dc66958e5bec2ec57602df2870479d42452795
KelvinSS <kelvimcyndder@gmail.com>
2024-10-02 18:58:43 -0300

    Pequena modificacao para exibir versao do app

--- src/components/ZenithName.js
+++ src/components/ZenithName.js
@@ -1,33 +1,32 @@
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
 import { COLOR, FONTE } from "../theme/Theme";
 import RText from './RText';
 
-export default function ZenithName() {
+export default function ZenithName({ showVersion = false }) {
     return (
         <View>
             <RText style={styles.textContainer}>
-                <RText style={styles.textColor}>Z</RText>enith
+                <RText style={styles.highlight}>Z</RText>enith
+                {showVersion && <RText style={styles.versionText}> V0.1</RText>}
             </RText>
         </View>
     );
 };
 
-const styles = {
+const styles = StyleSheet.create({
     textContainer: {
         fontSize: 40,
         marginBottom: 20,
-        paddingTop: "20%",
         alignSelf: 'center',
         justifyContent: 'center',
         fontFamily: FONTE.Bold,
     },
-    textColor: {
+    highlight: {
         color: COLOR.Jade,
         fontSize: 40,
-        marginBottom: 20,
-        paddingTop: "20%",
-        alignSelf: 'center',
-        justifyContent: 'center',
-        fontFamily: FONTE.Bold,
     },
-};
+    versionText: {
+        color: COLOR.Jade,
+        fontSize: 20,
+    },
+});
