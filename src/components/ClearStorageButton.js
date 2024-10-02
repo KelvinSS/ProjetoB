d462d8c0ab7f026f0ce6d8e15ac98a1dba4f2ca2
KelvinSS <kelvimcyndder@gmail.com>
2024-10-02 18:56:57 -0300

    Adicionando condicoes ao clicar

--- src/components/ClearStorageButton.js
+++ src/components/ClearStorageButton.js
@@ -1,20 +1,52 @@
-import React from 'react';
-import { View, Button, Alert } from 'react-native';
+import React, { useContext, useState } from 'react';
+import { View, Button, Alert, ActivityIndicator } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { AuthContext } from '../context/authContext';
+import { COLOR } from '../theme/Theme';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const ClearStorageButton = () => {
+    const { logout } = useContext(AuthContext);
+    const [isLoading, setIsLoading] = useState(false);
+    const navigation = useNavigation();
+
     const clearAsyncStorage = async () => {
-        try {
-            await AsyncStorage.clear();
-            Alert.alert('AsyncStorage limpo com sucesso!');
-        } catch (error) {
-            Alert.alert('Erro ao limpar o AsyncStorage:', error.message);
-        }
+        Alert.alert(
+            'Apagar tudo',
+            ` Deseja realmente apagar tudo?\n essa ação é permanente!\n Se sim, será necessario reiniciar o Zenith`,
+            [
+                { text: 'Cancelar', style: 'cancel' },
+                {
+                    text: 'Apagar',
+                    onPress: async () => {
+                        setIsLoading(true);
+                        try {
+                            await AsyncStorage.clear();
+                            await logout();
+                            navigation.replace('Login');
+                        } catch (error) {
+                            console.error('Erro ao limpar AsyncStorage:', error);
+                            Alert.alert('Erro', 'Não foi possível apagar os dados.');
+                        } finally {
+                            setIsLoading(false);
+                        }
+                    }
+                }
+            ]
+        );
     };
 
     return (
-        <View>
-            <Button title="Limpar AsyncStorage" onPress={clearAsyncStorage} />
+        <View style={{ marginTop: 10 }}>
+            {isLoading ? (
+                <ActivityIndicator size="large" color={COLOR.Red} />
+            ) : (
+                <Button
+                    title="Apagar TODOS os dados"
+                    onPress={clearAsyncStorage}
+                    color={COLOR.Red}
+                />
+            )}
         </View>
     );
 };
